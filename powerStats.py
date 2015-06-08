0ea888bac0b991cf32c45dd49d7f1157ea04cde6
Keaton Taylor <keatonstaylor@gmail.com>
2015-06-08 00:08:10 -0500

    Initial Release

---
+++ powerStats.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+'''
+
+Copyright (C) 2015 Keaton S. Taylor http://keatonstaylor.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Revision History:
+
+June 7th, 2015 Keaton Taylor	Initial Release. 
+
+'''
+
+import csv
+import serial
+import StringIO
+
+#SERIAL_PORT = '/dev/tty'		# Linux
+SERIAL_PORT = '/dev/tty.usbmodem1451'	# Mac/Unix
+
+try:
+    robot = serial.Serial(SERIAL_PORT, 115200, \
+        serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE,  1.0)
+except:
+    print('Unable to connect to Neato on serial port ' + SERIAL_PORT )
+    exit()
+
+robot.write('GetLifeStatLog\n') 
+
+foundRun = False
+
+print '+---------------------------------------------------------+'
+print '| Log Entry | Runtime (minutes) | mAh Used | Current Draw | '
+print '+---------------------------------------------------------+'
+
+while True:
+    line = robot.readline()
+    if line == chr(26):
+        break
+    for row in csv.reader(StringIO.StringIO(line)):
+        if len(row) == 7:
+            if row[1] == 'LS_RunDate':
+                foundRun = True
+            if foundRun == True:
+                if row[1] == 'LS_A2D1':
+                    runTime =  int(row[2])
+                elif row[1] == 'LS_A2D13':
+                    totalCurrent = int(row[5], 0)
+                    print '| ' + str(row[0]).ljust(10) + '| ' + str(runTime/6000).ljust(18) + '| ' \
+                        + str(totalCurrent/360000).ljust(9) + '| ' + str(totalCurrent/runTime).ljust(13) + '|'
+                    foundRun = False
+
+print '+---------------------------------------------------------+'
+robot.close()
